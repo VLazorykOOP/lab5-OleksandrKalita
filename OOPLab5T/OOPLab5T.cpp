058fa202937335ef941f0b607c0fd7bd2e1a0f99
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-05-01 13:44:02 +0200

    create struct commit
    create new struct for class People

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -1,11 +1,45 @@
-﻿#include<iostream>
-#include<cmath>
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <windows.h>
 
 using namespace std;
 
+struct PIB 
+{
+    string fname;
+    string name;
+    string lname;
+
+    PIB() {
+        fname = "NoFirstName";
+        name = "NoName";
+        lname = "NoLastName";
+        cout << "Default constructor PIB \n";
+    }
+
+    PIB(string fn) {
+        fname = "NoFirstName";
+        name = fn;
+        lname = "NoLastName";
+        cout << "Constructor PIB(string fn) \n";
+    }
+
+    PIB(string fn[3]) {
+        fname = fn[0];
+        name = fn[1];
+        lname = fn[2];
+        cout << "Constructor PIB(string fn[]) \n";
+    }    
+    ~PIB() {
+        cout << "Destructor ~PIB() \n";
+    }
+}
 
 int main(){
+    string n
 
-    
     return 0;
 }
+
