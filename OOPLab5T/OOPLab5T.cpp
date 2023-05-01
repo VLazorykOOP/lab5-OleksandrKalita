3365cb6907c5d859e9d7883f2010dcb64821f3e8
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-05-01 14:47:45 +0200

    person commit
    create class person

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -35,10 +35,58 @@ struct PIB
     ~PIB() {
         cout << "Destructor ~PIB() \n";
     }
+    string toString() {
+        string ts = "PIB: " + fname + " " + name + " " + lname + " ";
+        return ts;
+    }
+    void Input() {
+        cout << "Input fname "; cin >> fname;
+        cout << "Input name "; cin >> name;
+        cout << "Input lname "; cin >> lname;
+    }
 }
 
+class Person {
+    protected:
+        int age, weight;
+        PIB name;
+
+    public:
+    Person() {
+        age = 0;
+        weight = 1;
+        cout << " Default constructor Person() \n";
+    }
+    Person(int a, int w, string n) {
+        age = a;
+        weight = w;
+        name = n;
+        cout << " Constructor Person(int a, int w, string n) \n";
+    }
+    ~Person(){
+        cout << "Destructor ~Person() \n";
+    }
+    void setAge(int a) {
+        if (a >= 0 && a <= 200){
+           age = a; 
+        }
+        else{
+            cout << "Age " << a << " not allow, age is " << age << endl;;
+        }
+    }
+    string toString() {
+        string r,w,n ;
+        a = to_string(age);
+        w = to_string(weight);
+        r = name.toString() + " age: " + a + " weight: " + n;
+        return r;
+    }
+}
+
+
+
 int main(){
-    string n
+    
 
     return 0;
 }
